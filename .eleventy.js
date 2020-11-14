bb5b17822481321fe5015cc0a9fc88a0b79c1876
Adam Culpepper <adam@trendsic.com>
2020-11-14 06:51:11 -0600

    updating `path` to `src` and `classes` to `class`

--- .eleventy.js
+++ .eleventy.js
@@ -3,22 +3,22 @@ module.exports = function(eleventyConfig, pluginNamespace) {
 		eleventyConfig.addShortcode('respimg', parameter => {
 			var errors = '';
 
-			if (!parameter.path) {
-				errors += 'path parameter missing!';
+			if (!parameter.src) {
+				errors += 'src parameter missing!';
 			} else if (!parameter.sizes) {
 				errors += 'sizes parameter missing!';
 			}
 
 			if (errors) {
-				return '<span style="background:lightsalmon; padding:5px;">' + errors + '</span>';
+				return '<span style="background:lightsalmon; color:#fff; padding:5px;">' + errors + '</span>';
 			} else {
 				const hostname = eleventyConfig.hostname ? eleventyConfig.hostname : '';
 				const arraySizes = parameter.sizes.replace(/ /g,'').split(',');
 				const maxSize = Math.max.apply(Math, arraySizes);
 				const baseUrl = `https://res.cloudinary.com/${eleventyConfig.cloudinaryCloudName}/image/fetch/`;
-				const imageSrc = `${baseUrl}q_auto,f_auto,w_${maxSize}/${hostname}${parameter.path}`;
+				const imageSrc = `${baseUrl}q_auto,f_auto,w_${maxSize}/${hostname}${parameter.src}`;
 				const srcset = arraySizes.map(width => {
-					return `${baseUrl}q_auto,f_auto,w_${width}/${hostname}${parameter.path} ${width}w`;
+					return `${baseUrl}q_auto,f_auto,w_${width}/${hostname}${parameter.src} ${width}w`;
 				}).join(',');
 
 				return '<img ' +
@@ -28,7 +28,7 @@ module.exports = function(eleventyConfig, pluginNamespace) {
 					' src="' + imageSrc + '"' +
 					' srcset="' + srcset + '"' +
 					(parameter.alt ? ' alt="' + parameter.alt.trim() + '"' : '') +
-					(parameter.classes ? ' class="' + parameter.classes + '"' : '') +
+					(parameter.class ? ' class="' + parameter.class + '"' : '') +
 					(parameter.loading ? ' loading="' + parameter.loading + '"' : '') +
 					'>';
 			}
